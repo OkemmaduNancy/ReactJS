cf90bc1318e2741f3e3d960790a1fce883adcb6c
Okemmadu Nancy <anurika.okemmadu@gmail.com>
2021-03-16 14:44:13 +0000

    files committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import classes from './App.module.css';
+import ProductData from './ProductData';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className={classes.Topbar}>
+          <img src="https://m.media-amazon.com/images/I/31%2BDgxPWXtL.jpg" alt="Amazon Logo" />
+        </nav>
       </header>
+
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+
+          <div className={classes.FeatureData}>
+            <p>{`${new Date().getHours()}:${new Date().getMinutes}`}</p>
+          </div>
+        </div>
+
+        <div className={classes.ProductData}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDescription}>{ProductData.description}</p>
+          <h3 className={classes.sectionHeading}>Select Color</h3>
+          <div>
+            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black Colored Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Colored Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Colored Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Colored Watch" />
+          </div>
+          <h3 className={classes.sectionHeading}>Features</h3>
+          <div>
+            <button className={[classes.FeatureItem, classes.SelectedFeatureImage].join(' ')}>Time</button>
+            <button className={classes.FeatureItem}>Heart Rate</button>
+          </div>
+          <button className={classes.PrimaryButton}>Buy Now</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/ProductData.js
@@ -0,0 +1,27 @@
+const ProductData = {
+    title: 'FitBit 19 - The Smartest Watch',
+    description: 'This watch is Amazon product This watch is Amazon product This watch is Amazon product This watch is Amazon product.',
+    colorOption: [
+        {
+            styleName: 'Black Strap',
+            imageUrl: 'https://imgur.com/iOeUBV7.png'
+        },
+        {
+            styleName: 'Red Strap',
+            imageUrl: 'https://imgur.com/PTgQlim.png'
+        },
+        {
+            styleName: 'Blue Strap',
+            imageUrl: 'https://imgur.com/Mplj1YR.png'
+        },
+        {
+            styleName: 'Purple Strap',
+            imageUrl: 'https://imgur.com/xSIK4M8.png'
+        },
+    ],
+    featureList: [
+        "Time", "Heart Rate"
+    ]
+
+}
+export default ProductData;
